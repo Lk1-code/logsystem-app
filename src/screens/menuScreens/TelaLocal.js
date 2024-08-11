700ed110ec6ae69a943bbb563abba73026347196
Lucas Mariano Da Silva <lucasm1593@gmail.com>
2024-08-11 17:11:36 -0300

    -update telaConsulta

--- src/screens/menuScreens/TelaLocal.js
+++ src/screens/menuScreens/TelaLocal.js
@@ -1,26 +1,57 @@
-import { Text, View } from 'react-native'
-import React, { StyleSheet,Component, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 
-function TelaLocal({navigation}){
-  const [local,setLocal] = useState([]);
-  useEffect(async ()=>{
-    const localData = await firestore().collection('Estoque').doc(localOrigem).get();
-  })
+function TelaLocal({ route }) {
+  const [localData, setLocalData] = useState([]);
+  const { local } = route.params; //obtem a variavel do local armazenada na tela anterior
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const querySnapshot = await firestore()
+          .collection('Estoque')  // Acessa a coleção principal "Estoque"
+          .doc(local)             // Acessa o documento específico (passado via parâmetro)
+          .collection('Itens')    // Acessa a subcoleção "Itens" dentro do documento "local"
+          .get();
+
+        if (!querySnapshot.empty) {
+          const data = querySnapshot.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data(),
+          }));
+          setLocalData(data); // Armazena os dados do Local na Variavel
+        } else {
+          console.log('Nenhum item encontrado na subcoleção');
+        }
+      } catch (error) {
+        console.error('Erro ao buscar dados do Firestore: ', error);
+      }
+    }
+
+    getData();
+  }, [local]);
+
+  //verifica se ha dados na coleção
+  if (localData.length === 0) {
+    return (
+      <View>
+        <Text>Carregando dados...</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-        <Text style={styles.title}>Lista de Vacinas</Text>
-        <FlatList
-            data={vacinas}
-            renderItem={({ item }) => 
-            <Text style={styles.item}>
-            Nome do Pet: {item.nomePet}{'\n'}
-            Data de Nascimento: {item.DataNasc}{'\n'}
-            Vacina:{item.nomeVacina}{'\n'}
-            Data da vacina: {item.DataVac}</Text>}
-            keyExtractor={(item) => item.id.toString()}
-        />
+    <View>
+      {localData.map(item => (
+        <View key={item.id}>
+          <Text>ID do item: {item.id}</Text>
+          <Text>Descrição: {item.Produto}</Text>
+          <Text>Quantidade: {item.quantidade}</Text>
+        </View>
+      ))}
     </View>
   );
-};
+}
+
 export default TelaLocal;
